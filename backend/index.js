5d1e16168506d72d89253bc84706c5d838ca1eb0
Pablo <larrazjp@gmail.com>
2023-10-27 10:33:36 +0200

    Merge del frontend

--- backend/index.js
+++ backend/index.js
@@ -7,20 +7,20 @@ const port = 3000
 
 app.get('/', async (req, res) => {
   try {
-    const nuevoUsuario = new UsuarioVO();
-    nuevoUsuario.setIdUser(1)
-    nuevoUsuario.setNombreUsuario("hola");
-    nuevoUsuario.setFoto("hola");
-    nuevoUsuario.setCorreo("hola");
-    nuevoUsuario.setContraseña("hola");
+    // const nuevoUsuario = new UsuarioVO();
+    // nuevoUsuario.setIdUser(1)
+    // nuevoUsuario.setNombreUsuario("hola");
+    // nuevoUsuario.setFoto("hola");
+    // nuevoUsuario.setCorreo("hola");
+    // nuevoUsuario.setContraseña("hola");
 
-    const resultado = await validarUsuario(nuevoUsuario);
-    if (resultado.rows[0].exists) {
-        console.log('El usuario existe en la base de datos.');
-      } else {
-        console.log('El usuario no existe en la base de datos.');
-      }
-      res.send("usuario validado");
+    // const resultado = await validarUsuario(nuevoUsuario);
+    // if (resultado.rows[0].exists) {
+    //     console.log('El usuario existe en la base de datos.');
+    //   } else {
+    //     console.log('El usuario no existe en la base de datos.');
+    //   }
+      res.send("HOLA MUNDO!");
   } catch (error) {
     console.error('Error al crear usuario:', error);
     res.status(500).send('Error al crear usuario');
