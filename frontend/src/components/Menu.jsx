bcbca887c8fd64f1502dc7da320b7993fcf0ca1f
miguelius03 <127440837+miguelius03@users.noreply.github.com>
2023-11-03 16:47:39 +0100

    Add files via upload

---
+++ frontend/src/components/Menu.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Estilos/Menu.css'
+//import './TuComponente.css'; // Asegúrate de importar tu archivo CSS o usar estilos en línea
+
+function Menu() {
+  return (
+    <div className="barra-horizontal">
+      <div className="mis-listas">
+        <i className="icono-mis-listas">&#9776;</i> MIS LISTAS
+      </div>
+      <div className="crear-lista">
+        <i className="icono-crear-lista">+</i> CREAR LISTA
+      </div>
+      <div className="social">
+        <i className="icono-social">&#9733;</i> SOCIAL
+      </div>
+    </div>
+  );
+}
+
+export default Menu;
