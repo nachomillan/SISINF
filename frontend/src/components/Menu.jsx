aac3d8615c8cb1d1188bbfa2a40193590b8b978e
Pablo <larrazjp@gmail.com>
2023-11-10 15:31:07 +0100

    hika

--- frontend/src/components/Menu.jsx
+++ frontend/src/components/Menu.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import CrearLista from './CrearLista'; // Import the CrearLista component
 import './Estilos/Menu.css';
-
+import { useNavigate } from 'react-router-dom';
 function Menu() {
+  const navigate = useNavigate();
   const [crearListaVisible, setCrearListaVisible] = useState(false); // State to control the visibility of CrearLista
 
   // Function to show the CrearLista panel
@@ -24,9 +25,10 @@ function Menu() {
       <div className="crear-lista" onClick={showCrearLista}>
         <i className="icono-crear-lista">+</i> CREAR LISTA
       </div>
-      <div className="social">
-        <i className="icono-social">&#9733;</i> SOCIAL
+      <div className="social" onClick={navigate('/social')}>
+        <i className="icono-social" onClick={navigate('/social')}>&#9733;</i> SOCIAL
       </div>
+
       {crearListaVisible && (
         <div className="crear-lista-overlay">
           <CrearLista onClose={hideCrearLista} />
